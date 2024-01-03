83468ac6a6a07f1c51271686bb279555ce448096
Olena Korenieva <olena.korenieva@gmail.com>
2024-01-03 23:47:01 +0100

    resolved conflict after pulling main branch

--- frontend/src/pages/login/LoginPage.jsx
+++ frontend/src/pages/login/LoginPage.jsx
@@ -1,14 +1,41 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import "./LoginPage.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { login } from "../../redux/auth/authOperations";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleLogin = () => {
-    console.log(`Login with email address: ${email} and password: ${password}`); //authentication
+  const resetform = () => {
+    setEmail("");
+    setPassword("");
   };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log(e);
+    console.log(`Login with email address: ${email} and password: ${password}`); //authentication
+    const currentUser = {
+      email: e.target.elements.email.value,
+      password: e.target.elements.password.value,
+    };
+    console.log(currentUser);
+    const response = await dispatch(login(currentUser));
+    if (response.error) {
+      console.log(response.payload);
+    } else {
+      console.log("Success");
+      navigate("/home");
+    }
+    resetform("");
+  };
+
+  console.log(`Login with email address: ${email} and password: ${password}`);
 
   return (
     <div className="login-container">
@@ -30,14 +57,15 @@ const LoginPage = () => {
             <div className="col mt-3">
               <div className="row">
                 <div className="col">
-                  <form className="form-text">
-                    <div className="mb-2">
+                  <form onSubmit={(e) => handleLogin(e)}>
+                    <div className="mb-3">
                       <label htmlFor="email" className="form-label">
                         Email Address:
                       </label>
                       <input
+                        name="email"
                         type="email"
-                        className="form-control custom-input"
+                        className="form-control"
                         id="email"
                         placeholder="Type Email Address"
                         value={email}
@@ -49,25 +77,22 @@ const LoginPage = () => {
                         Password:
                       </label>
                       <input
+                        name="password"
                         type="password"
-                        className="form-control custom-input"
+                        className="form-control"
                         id="password"
                         placeholder="Type Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                       />
                     </div>
-                    <button
-                      onClick={handleLogin}
-                      type="submit"
-                      className="btn btn-primary w-100"
-                    >
+                    <button type="submit" className="btn btn-primary w-100">
                       Login
                     </button>
                   </form>
                   <div className="row mt-3">
                     <div className="col">
-                      Don't have an account? <a href="/signup">Sign Up</a>
+                      Don't have an account? <a href="#">Sign Up</a>
                     </div>
                   </div>
                 </div>
